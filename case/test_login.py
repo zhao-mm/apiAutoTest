14381b641f14bc7152b726a0772f55486a4d88f1
zhao-mm <zhaoyc19951217@126.com>
2020-06-27 19:11:09 +0800

    read_json

--- case/test_login.py
+++ case/test_login.py
@@ -5,14 +5,29 @@
 # 导包 unittest ApiLogin
 import unittest
 from api.api_login import ApiLogin
+from parameterized import parameterized
+from tools.read_json import ReadJson
+
+# 读取数据函数
+def get_data():
+    data = ReadJson("login.json").read_json()
+    arrs = []
+    arrs.append((data.get("url"),
+                data.get("mobile"),
+                data.get("code"),
+                data.get("expect_result"),
+                data.get("status_code")))
+    return arrs
+
 # 新建测试类
 class TestLogin(unittest.TestCase):
     # 新建测试方法
-    def test_login(self):
+    @parameterized.expand(get_data())
+    def test_login(self, url, mobile, code, expect_result, status_code):
         # 暂时存放数据 url mobile code
-        url = "http://ttapi.research.itcast.cn/app/v1_0/authorizations"
-        mobile = "18717852993"
-        code = "419104"
+        # url = "http://ttapi.research.itcast.cn/app/v1_0/authorizations"
+        # mobile = "18717852993"
+        # code = "419104"
 
         # 调用登录方法
         s = ApiLogin().api_post_login(url, mobile, code)
@@ -21,10 +36,10 @@ class TestLogin(unittest.TestCase):
         print(s.json())
 
         # 断言 响应信息 及 状态码
-        self.assertEqual("OK", s.json()['message'])
+        self.assertEqual(expect_result, s.json()['message'])
 
         # 断言响应状态码
-        self.assertEqual(201, s.status_code)
+        self.assertEqual(status_code, s.status_code)
 
 
 # 获取验证码code
---
+++ tools/read_json.py
@@ -0,0 +1,11 @@
+﻿import json
+
+class ReadJson(object):
+
+    def __init__(self, filename):
+        self.filepath = "../data/" + filename
+
+    def read_json(self):
+        with open(self.filepath, "r", encoding="utf-8-sig") as f:
+            # 调用load方法加载文件流
+            return json.load(f)
